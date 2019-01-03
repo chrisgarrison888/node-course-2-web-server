0779821aebdebabd32bd3db11b66c1a312e78ba9
Garrison <cgarrison3@cgarrison-MBP-TB.local>
2019-01-03 14:05:22 -0600

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -46,6 +47,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on 30000');
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
 });
